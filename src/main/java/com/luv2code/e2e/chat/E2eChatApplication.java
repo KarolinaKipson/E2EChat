05dcb760cdd31b53908d62db794171388b1b9960
Luka Žugaj <lzugaj@tvz.hr>
2019-12-21 15:08:45 +0100

    Initial commit

---
+++ src/main/java/com/luv2code/e2e/chat/E2eChatApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.e2e.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2eChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2eChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luv2code/e2e/chat/E2eChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.e2e.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2eChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
